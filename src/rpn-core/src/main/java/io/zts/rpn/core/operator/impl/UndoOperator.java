42c86bff3f23dec1c8b835d13795bc72c0392967
songdragon <lovedragonchina@gmail.com>
2019-10-19 21:13:00 +0800

    #2 implement undo operation with simple test case

--- src/rpn-core/src/main/java/io/zts/rpn/core/operator/impl/UndoOperator.java
+++ src/rpn-core/src/main/java/io/zts/rpn/core/operator/impl/UndoOperator.java
@@ -1,14 +1,28 @@
 package io.zts.rpn.core.operator.impl;
 
+import io.zts.rpn.core.domain.HistoryOperationStack;
 import io.zts.rpn.core.domain.Operation;
+import io.zts.rpn.core.domain.RpnNumber;
 import io.zts.rpn.core.domain.RpnStack;
 import io.zts.rpn.core.exception.InsufficientParametersException;
 import io.zts.rpn.core.operator.Operator;
+
+import java.util.List;
 
 public class UndoOperator implements Operator {
     @Override
     public Operation operate(RpnStack stack) throws InsufficientParametersException {
         stack.pop();
         return null;
+    }
+
+    public void recoverStack(RpnStack stack, HistoryOperationStack historyStack){
+        Operation operation=historyStack.pop();
+        List<RpnNumber> numbers = operation.getNumbers();
+        if(numbers!=null){
+            for(RpnNumber num:numbers){
+                stack.push(num);
+            }
+        }
     }
 }
---
+++ src/rpn-core/src/test/java/io/zts/rpn/core/operator/UndoOperatorTestCase.java
@@ -0,0 +1,44 @@
+package io.zts.rpn.core.operator;
+
+import io.zts.rpn.core.domain.HistoryOperationStack;
+import io.zts.rpn.core.domain.Operation;
+import io.zts.rpn.core.domain.RpnNumber;
+import io.zts.rpn.core.domain.RpnStack;
+import io.zts.rpn.core.exception.InsufficientParametersException;
+import io.zts.rpn.core.operator.impl.AdditionOperator;
+import io.zts.rpn.core.operator.impl.UndoOperator;
+import org.junit.Before;
+import org.junit.Test;
+
+import static junit.framework.Assert.assertNull;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+public class UndoOperatorTestCase {
+
+    RpnStack stack=new RpnStack();
+    UndoOperator undoOp=new UndoOperator();
+    HistoryOperationStack historyOperationStack=new HistoryOperationStack();
+
+    @Before
+    public void before(){
+        stack.clear();
+        historyOperationStack.clear();
+    }
+
+    @Test
+    public void testUndoOp() throws InsufficientParametersException {
+
+        stack.push(new RpnNumber("3"));
+        stack.push(new RpnNumber("2"));
+
+        Operation oldOperation=new Operation(RpnOperator.SQRT,new RpnNumber("123"));
+        historyOperationStack.push(oldOperation);
+
+        Operation operation = undoOp.operate(stack);
+        undoOp.recoverStack(stack,historyOperationStack);
+        assertEquals("stack: 3 123", stack.toString());
+        assertNull(operation);
+    }
+
+}
